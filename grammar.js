51e1e5c9785165630f3c164d9ba703216435bf9b
Nicholas Blott <blottn@tcd.ie>
2019-02-28 11:29:40 +0000

    added initial grammar

---
+++ grammar.js
@@ -0,0 +1,31 @@
+function Sequence(a,b) {
+    this.first = a;
+    this.second = b;
+
+    this.match = function(input, callback) {
+        callback(a.match(input, b.match));
+    }
+}
+
+
+// primitives
+function Empty() {
+    this.match = function(input, callback) {
+        return input;
+    }
+}
+
+function Terminal(string) {
+    this.terminal = string;
+    this.match = function(input, callback) {
+        if (input.startsWith(this.terminal)) {
+            return input.substring(string.length);
+        }
+        else {
+            // error until matched
+            // something todo with continue from here
+            input.indexOf(this.terminal);
+        }
+    }
+}
+
